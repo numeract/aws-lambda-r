76e92092e9620f85eafb9231535a46aabe45624a
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2017-12-06 01:10:27 +0200

    Renamed ami script

--- scripts/00_main_setup.sh
+++
@@ -1,9 +0,0 @@
-#/!bin/bash
-
-# load local settings if not already loaded
-[[ $SCR_DIR ]] || SCR_DIR="$(cd "$(dirname "$0")/."; pwd)"
-[[ $PRJ_DIR ]] || source "$SCR_DIR/02_setup.sh"
-
-
-
-source "$SCR_DIR/00_aws_setup.sh"
