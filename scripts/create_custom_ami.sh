87ac7dccc8e2bc313af81d68ee130c9ec0c5bb49
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2017-12-06 13:32:28 +0200

    Check AMI state

--- scripts/create_custom_ami.sh
+++ scripts/create_custom_ami.sh
@@ -13,4 +13,22 @@ source "$SCR_DIR/15_stop_ec2.sh"
 
 EC2_AMI_ID=$(aws ec2 create-image --instance-id $EC2_INSTANCE_ID --name "Custom AMI" --output text)
 
+# Wait until the image is available
+echo -e "$INFO Waiting for the AMI to be available ..."
+OVER=0
+TEST=0
+while [[ $OVER -eq 0 ]] && [[ $TEST -lt $EC2_MAX_TESTS ]]; do
+    AMI_STATE=$(aws ec2 describe-images --image-ids $EC2_AMI_ID --query Images[0].State --output text)
+    if [[ "$AMI_STATE" == "available" ]]; then
+        OVER=1
+    else
+        TEST=$(( TEST+1 ))
+        echo -e "$INFO Check # $(FC $TEST). Trying again in 10 seconds. Please wait ..."
+        sleep 10
+    fi
+done
+ 
+
 echo -en "\nEC2_AMI_ID=${EC2_AMI_ID}"|  tee -a ../settings/default_setup.sh
+
+source "$SCR_DIR/08_terminate_ec2.sh"
