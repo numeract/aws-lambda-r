bf241d167dac683ae745a2143acc8b161ae06253
Mike Badescu <mike.badescu@numeract.com>
2018-03-06 20:10:44 -0600

    setup s3

--- scripts/22_setup_s3.sh
+++ scripts/22_setup_s3.sh
@@ -2,38 +2,48 @@
 
 # check if S3 bucket exists given the config variables, create it if not
 
+
 # load local settings if not already loaded
 [[ $SCR_DIR ]] || SCR_DIR="$(cd "$(dirname "$0")/."; pwd)"
 [[ $PRJ_DIR ]] || source "$SCR_DIR/02_setup.sh"
 
 
-# check given name
+# if no given name, use a name = last 4 letters of access key and prj name
 if [[ $S3_BUCKET == "$MISSING" ]]; then
     S3_BUCKET=""$(printf $IAM_ACCESS_KEY_ID | tail -c 4)"-${PRJ_NAME}"
+    S3_BUCKET="$(echo ${S3_BUCKET} | tr '[A-Z]' '[a-z]')"
 fi
+echo -e "$INFO S3 Bucket to use: $(FC $S3_BUCKET)"
 
-echo -e "S3 Bucket for the deployment package is: $(FC $S3_BUCKET)"
 
-exit 1
-
-# now we have what we believe to be a valid S3 name; does the bucket exist?
-aws $AWS_PRFL s3api wait bucket-exists \
-    --bucket ${S3_BUCKET}
-exit_status=$?
-if [[ $exit_status -ne 0 ]]; then
-    OVER=1
+# check existence
+if aws $AWS_PRFL s3 ls "s3://${S3_BUCKET}" 2>&1 | grep -q 'NoSuchBucket'; then
+    echo -e "$INFO S3 Bucket $(FC $S3_BUCKET) does not exist. Creating it ..."
 else
-    TEST=$(( TEST+1 ))
-    echo -e "$INFO Check # $(FC $TEST). Trying again in 5 seconds. Please wait..."
-    sleep 5
+    echo -e "$ERROR S3 Bucket $(FC $S3_BUCKET) already exists. Exiting."
+    exit 1
 fi
 
 
-# TODO: check S3 exists given current name, ignore missing
+# creating S3 Bucket
+if [[ $AWS_REGION == "us-east-1" ]]; then
+    aws $AWS_PRFL s3api create-bucket \
+        --bucket ${S3_BUCKET} \
+        --region ${AWS_REGION} \
+        --output table
+else    
+    aws $AWS_PRFL s3api create-bucket \
+        --bucket ${S3_BUCKET} \
+        --region ${AWS_REGION} \
+        --create-bucket-configuration LocationConstraint=${AWS_REGION} \
+        --output table
+fi
 
 
-# Creating S3 Bucket
-aws $AWS_PRFL s3api create-bucket \
-    --bucket ${S3_BUCKET} \
-    --region ${AWS_REGION} \
-    --create-bucket-configuration LocationConstraint=${AWS_REGION}
+# test existence
+if aws $AWS_PRFL s3 ls "s3://${S3_BUCKET}" 2>&1 | grep -q 'NoSuchBucket'; then
+    echo -e "$ERROR S3 Bucket $(FC $S3_BUCKET) still does not exist." \
+        "Please create it from AWS Web Console."
+else
+    echo -e "$INFO S3 Bucket $(FC $S3_BUCKET) now exists."
+fi
