f8192d8653a449763c8cc4c0446951879f04d352
Mike Badescu <mike.badescu@numeract.com>
2018-03-08 21:28:24 -0600

    update ec2 config

--- scripts/12_configure_ec2.sh
+++ scripts/12_configure_ec2.sh
@@ -1,50 +1,24 @@
 #!/bin/bash
 
-# run on EC2 to configure it
+# run on EC2, configure EC2
 
 
-# variable & functions to display messages
-INFO="\e[32mINFO :\e[39m"                               # Green
-WARN="\e[33mWARN :\e[39m"                               # Yellow
-ERROR="\e[31mERROR:\e[39m"                              # Red
-MISSING="\e[95mMISSING\e[39m"                           # Magenta
-
-FY () { echo -e "\e[33m$1\e[39m"; }                     # Foreground Yellow
-FC () { echo -e "\e[36m$1\e[39m"; }                     # Foreground Cyan
-BY () { echo -e "\e[43m\e[30m$1\e[39m\e[49m"; }         # Background Yellow
+# All 4 settings scripts were called before calling this scrip
+# We have access to all the settings as on the local machine
 
 
-# create AWS config files
+# on EC2 we need to connect to other AWS service (S3, Lambda, API Gateway, ...)
+# to simplify use of AWS CLI on EC2 we will create AWS config files
 echo -e "$INFO Configure AWS on EC2"
 sudo mkdir -p ~/.aws
 echo -en \
     "[default]\n" \
     "output = json\n" \
-    "region = ${AWS_REGION}\n" | sudo tee ~/.aws/config
+    "region = ${AWS_REGION}\n" | \
+    sudo tee ~/.aws/config
 echo -en \
     "[default]\n" \
     "aws_access_key_id = ${IAM_ACCESS_KEY_ID}\n" \
     "aws_secret_access_key = ${IAM_SECRET_ACCESS_KEY}\n" \
-    "region = ${AWS_REGION}\n" | sudo tee ~/.aws/credentials
-
-# The Name of the resource under the API Gateway
-API_RESOURCE_NAME="$(aws apigateway get-resource \
-    --rest-api-id ${API_ID} \
-    --resource-id ${API_RESOURCE_ID} \
-    --query "pathPart" \
-    --output text)"
-echo -e "$INFO API_RESOURCE_NAME: $(FC $API_RESOURCE_NAME)"
-
-# The Name of the resource alias under the API Gateway
-API_ALIAS_RESOURCE_NAME="$(aws apigateway get-resource \
-    --rest-api-id ${API_ID} \
-    --resource-id ${API_ALIAS_RESOURCE_ID} \
-    --query "pathPart" \
-    --output text)"
-echo -e "$INFO API_ALIAS_RESOURCE_NAME: $(FC $API_ALIAS_RESOURCE_NAME)"
-
-
-# arbitrary AWS Lambda function name - defined here too, to be available on EC2
-LAMBDA_FUNCTION_NAME="${PRJ_NAME}-${PRJ_BRANCH}-${API_STAGE}"
-LAMBDA_ZIP="${LAMBDA_FUNCTION_NAME}.zip"
-echo -e "$INFO LAMBDA_FUNCTION_NAME: $(FC $LAMBDA_FUNCTION_NAME)"
+    "region = ${AWS_REGION}\n" | \
+    sudo tee ~/.aws/credentials
