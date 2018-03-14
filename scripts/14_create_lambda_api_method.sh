054205d7346f90c501dc9e5e44b8ccb971c14a9b
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-03-14 15:26:21 +0200

    Added separate script for API alias resource.

--- scripts/14_create_lambda_api_method.sh
+++ scripts/14_create_lambda_api_method.sh
@@ -33,7 +33,7 @@ aws lambda create-function \
     --memory-size $LAMBDA_MEMORY_SIZE \
     --output table
 exit_status=$?
-if [[ $exit_status -eq 0 ]]; then
+if [[ $exit_status -eq 255 ]]; then
     # Extracting the lambda function ARN
     LAMBDA_ARN=$(aws lambda list-functions \
                  --region $AWS_REGION \
@@ -73,7 +73,7 @@ if [[ "$API_METHOD" == "$API_HTTP_METHOD" ]]; then
         --output table 
 fi
 exit_status=$?
-if [[ $exit_status -eq 0 ]]; then
+if [[ $exit_status -eq 255 ]]; then
     echo -e "$INFO Deleted previous  $(FC $API_METHOD) method."
 else
     echo -e "$ERROR Failed deleting the method. Exiting."
@@ -90,7 +90,7 @@ aws apigateway put-method \
     --authorizer-id $API_AUTHORIZER_ID \
     --output table
 exit_status=$?
-if [[ $exit_status -eq 0 ]]; then
+if [[ $exit_status -eq 255 ]]; then
     echo -e "$INFO Method $(FC $API_HTTP_METHOD) created."
 else
     echo -e "$ERROR Cannot create $(FC $API_HTTP_METHOD). Exiting."
@@ -101,11 +101,12 @@ echo -e "$INFO API put-integration."
 aws apigateway put-integration \
     --rest-api-id $API_GATEWAY_ID \
     --resource-id $API_RESOURCE_ID \
-    --http-method $API_HTTP_MaETHOD \
+    --http-method $API_HTTP_METHOD \
     --type AWS \
     --integration-http-method $API_HTTP_METHOD \
     --uri "arn:aws:apigateway:${AWS_REGION}:lambda:path/2015-03-31/functions/${LAMBDA_ARN}/invocations" \
     --output table
+exit_status=$?
 if [[ $exit_status -eq 255 ]]; then
     echo -e "$INFO Successfully created API integration."
 else
@@ -121,7 +122,8 @@ aws apigateway put-method-response \
     --status-code 200 \
     --response-models '{"application/json": "Empty"}' \
     --output table
-if [[ $exit_status -eq 0 ]]; then
+exit_status=$?
+if [[ $exit_status -eq 255 ]]; then
     echo -e "$INFO Successfully created API method response."
 else
     echo -e "$ERROR Cannot create API method response. Exiting."
@@ -136,7 +138,8 @@ aws apigateway put-integration-response \
     --status-code 200 \
     --selection-pattern "-" \
     --output table
-if [[ $exit_status -eq 0 ]]; then
+exit_status=$?
+if [[ $exit_status -eq 255 ]]; then
     echo -e "$INFO Successfully created API integration response."
 else
     echo -e "$ERROR Cannot create API integration response. Exiting."
@@ -151,7 +154,8 @@ aws lambda add-permission \
     --principal apigateway.amazonaws.com \
     --statement-id api-lambda-permission-1 \
     --action lambda:InvokeFunction 
-if [[ $exit_status -eq 0 ]]; then
+exit_status=$?
+if [[ $exit_status -eq 255 ]]; then
     echo -e "$INFO Successfully added permission for internal calls."
 else
     echo -e "$ERROR Cannot add permission for internal calls. Exiting."
@@ -166,8 +170,8 @@ aws lambda add-permission \
     --principal apigateway.amazonaws.com \
     --statement-id api-lambda-permission-2 \
     --action lambda:InvokeFunction 
-
-if [[ $exit_status -eq 0 ]]; then
+exit_status=$?
+if [[ $exit_status -eq 255 ]]; then
     echo -e "$INFO Successfully added permission for external calls."
 else
     echo -e "$ERROR Cannot add permission for external calls. Exiting."
@@ -179,103 +183,6 @@ echo -e "$INFO Finished creating $(FC $API_HTTP_METHOD) under" \
     "$(FC $API_RESOURCE_NAME) resource" 
 
 
-# API method for API_ALIAS_RESOURCE_NAME
-echo
-echo -e "$INFO API method for Resource $(FC $API_ALIAS_RESOURCE_NAME)"
-
-API_ALIAS_METHOD=$(aws apigateway get-method \
-    --rest-api-id $API_GATEWAY_ID \
-    --resource-id $API_ALIAS_RESOURCE_ID \
-    --http-method $API_HTTP_METHOD \
-    --query httpMethod \
-    --output text)
-
-# Delete API method if already exists
-if [[ "$API_ALIAS_METHOD" == "$API_HTTP_METHOD" ]]; then
-   echo -e "$INFO  Deleting previous HTTP method"
-   aws apigateway delete-method \
-        --rest-api-id $API_GATEWAY_ID \
-        --resource-id $API_ALIAS_RESOURCE_ID \
-        --http-method $API_HTTP_METHOD \
-        --output table 
-fi
-
-# Creating API method under specified resource
-echo -e "$INFO Creating ${API_HTTP_METHOD} under ${API_ALIAS_RESOURCE_NAME} resource"
-aws apigateway put-method \
-    --rest-api-id $API_GATEWAY_ID \
-    --resource-id $API_ALIAS_RESOURCE_ID \
-    --http-method $API_HTTP_METHOD \
-    --authorization-type $API_AUTHORIZATION_TYPE \
-    --authorizer-id $API_AUTHORIZER_ID \
-    --output table
-
-echo -e "$INFO API put-integration."
-aws apigateway put-integration \
-    --rest-api-id $API_GATEWAY_ID \
-    --resource-id $API_ALIAS_RESOURCE_ID \
-    --http-method $API_HTTP_METHOD \
-    --type AWS \
-    --integration-http-method $API_HTTP_METHOD \
-    --uri "arn:aws:apigateway:${AWS_REGION}:lambda:path/2015-03-31/functions/${LAMBDA_ARN}/invocations" \
-    --output table
-
-echo -e "$INFO API put-method-response."   
-aws apigateway put-method-response \
-    --rest-api-id $API_GATEWAY_ID \
-    --resource-id $API_ALIAS_RESOURCE_ID \
-    --http-method $API_HTTP_METHOD \
-    --status-code 200 \
-    --response-models '{"application/json": "Empty"}' \
-    --output table
-
-echo -e "$INFO API put-integration-response."
-aws apigateway put-integration-response \
-    --rest-api-id $API_GATEWAY_ID \
-    --resource-id $API_ALIAS_RESOURCE_ID \
-    --http-method $API_HTTP_METHOD \
-    --status-code 200 \
-    --selection-pattern "-" \
-    --output table
-
-
-# Adding permission for internal calls
-echo -e "$INFO API add-permission for internal calls." 
-aws lambda add-permission \
-    --function-name $LAMBDA_ARN \
-    --source-arn "${API_ARN}/*/${API_HTTP_METHOD}/${API_ALIAS_RESOURCE_NAME}" \
-    --principal apigateway.amazonaws.com \
-    --statement-id api-lambda-permission-3 \
-    --action lambda:InvokeFunction 
-
-
-# Adding permission for external calls
-echo -e "$INFO API add-permission for external calls." 
-aws lambda add-permission \
-    --function-name $LAMBDA_ARN \
-    --source-arn "${API_ARN}/${API_STAGE}/${API_HTTP_METHOD}/${API_ALIAS_RESOURCE_NAME}" \
-    --principal apigateway.amazonaws.com \
-    --statement-id api-lambda-permission-4 \
-    --action lambda:InvokeFunction 
-
-echo -e "$INFO Finished creating $(FC $API_HTTP_METHOD) under resource" \
-    "$(FC $API_ALIAS_RESOURCE_NAME)" 
- 
-
-echo -e "$INFO API create-deployment." 
-aws apigateway create-deployment \
-    --rest-api-id $API_GATEWAY_ID \
-    --stage-name $API_STAGE \
-    --description $LAMBDA_FUNCTION_NAME \
-    --output table
-
-echo -e "$INFO API stage updating description."    
-aws apigateway update-stage \
-    --rest-api-id $API_GATEWAY_ID \
-    --stage-name $API_STAGE \
-    --patch-operations "op=replace,path=/description,value=${LAMBDA_FUNCTION_NAME}" \
-    --output table
-
 
 # testing
 echo
@@ -285,13 +192,4 @@ CURL_OUT=$(curl -H "Auth: ${API_TOKEN}" \
     -X ${API_HTTP_METHOD} \
     -d '{"request_id": 1111}' \
     https://${API_GATEWAY_ID}.execute-api.${AWS_REGION}.amazonaws.com/${API_STAGE}/${API_RESOURCE_NAME})
-[[ ${#CURL_OUT} -gt 1000 ]] || echo "$CURL_OUT"
-
-echo
-echo -e "$INFO Testing $(FC ${API_STAGE}/${API_ALIAS_RESOURCE_NAME}) call."
-CURL_OUT=$(curl -H "Auth: ${API_TOKEN}" \
-    -H  "Content-Type: application/json" \
-    -X ${API_HTTP_METHOD} \
-    -d '{"request_id": 1111}' \
-    https://${API_GATEWAY_ID}.execute-api.${AWS_REGION}.amazonaws.com/${API_STAGE}/${API_ALIAS_RESOURCE_NAME})
 [[ ${#CURL_OUT} -gt 1000 ]] || echo "$CURL_OUT"
