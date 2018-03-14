64dfb6079d4d93cd1aa83424e2f8fbf609445747
AnaNiculescu36 <anamaria.niculescu36@gmail.com>
2018-03-14 15:03:03 +0200

    Checked if API commands are successfull

--- scripts/14_create_lambda_api_method.sh
+++ scripts/14_create_lambda_api_method.sh
@@ -32,14 +32,18 @@ aws lambda create-function \
     --timeout $LAMBDA_TIMEOUT \
     --memory-size $LAMBDA_MEMORY_SIZE \
     --output table
-
-# Extracting the lambda function ARN
-LAMBDA_ARN=$(aws lambda list-functions \
-    --region $AWS_REGION \
-    --query "Functions[?FunctionName==\`${LAMBDA_FUNCTION_NAME}\`].FunctionArn" \
-    --output text)
-echo -e "$INFO LAMBDA_ARN: $(FY $LAMBDA_ARN)"
-
+exit_status=$?
+if [[ $exit_status -eq 0 ]]; then
+    # Extracting the lambda function ARN
+    LAMBDA_ARN=$(aws lambda list-functions \
+                 --region $AWS_REGION \
+                 --query "Functions[?FunctionName==\`${LAMBDA_FUNCTION_NAME}\`].FunctionArn" \
+                 --output text)
+    echo -e "$INFO LAMBDA_ARN is: $(FY $LAMBDA_ARN)"
+else
+    echo -e "$ERROR Cannot create Lambda function. Exiting."
+    exit 1
+fi
 
 # AWS CLI cannot return API Gateway ARN; construct it
 API_ARN="arn:aws:execute-api"
@@ -49,7 +53,6 @@ echo -e "$INFO API_ARN: $(FY $API_ARN)"
 
 
 # API method for API_RESOURCE_NAME
-echo
 echo -e "$INFO API method for Resource $(FC $API_RESOURCE_NAME)"
 
 API_METHOD=$(aws apigateway get-method \
@@ -69,7 +72,13 @@ if [[ "$API_METHOD" == "$API_HTTP_METHOD" ]]; then
         --http-method $API_HTTP_METHOD \
         --output table 
 fi
-
+exit_status=$?
+if [[ $exit_status -eq 0 ]]; then
+    echo -e "$INFO Deleted previous  $(FC $API_METHOD) method."
+else
+    echo -e "$ERROR Failed deleting the method. Exiting."
+    exit 1
+fi
 
 # Creating API method under specified resource
 echo -e "$INFO Creating ${API_HTTP_METHOD} under ${API_RESOURCE_NAME} resource"
@@ -80,6 +89,13 @@ aws apigateway put-method \
     --authorization-type $API_AUTHORIZATION_TYPE \
     --authorizer-id $API_AUTHORIZER_ID \
     --output table
+exit_status=$?
+if [[ $exit_status -eq 0 ]]; then
+    echo -e "$INFO Method $(FC $API_HTTP_METHOD) created."
+else
+    echo -e "$ERROR Cannot create $(FC $API_HTTP_METHOD). Exiting."
+    exit 1
+fi
 
 echo -e "$INFO API put-integration."
 aws apigateway put-integration \
@@ -90,6 +106,12 @@ aws apigateway put-integration \
     --integration-http-method $API_HTTP_METHOD \
     --uri "arn:aws:apigateway:${AWS_REGION}:lambda:path/2015-03-31/functions/${LAMBDA_ARN}/invocations" \
     --output table
+if [[ $exit_status -eq 0 ]]; then
+    echo -e "$INFO Successfully created API integration."
+else
+    echo -e "$ERROR Cannot create API integration. Exiting."
+    exit 1
+fi
 
 echo -e "$INFO API put-method-response."
 aws apigateway put-method-response \
@@ -99,6 +121,12 @@ aws apigateway put-method-response \
     --status-code 200 \
     --response-models '{"application/json": "Empty"}' \
     --output table
+if [[ $exit_status -eq 0 ]]; then
+    echo -e "$INFO Successfully created API method response."
+else
+    echo -e "$ERROR Cannot create API method response. Exiting."
+    exit 1
+fi
 
 echo -e "$INFO API put-integration-response." 
 aws apigateway put-integration-response \
@@ -108,7 +136,12 @@ aws apigateway put-integration-response \
     --status-code 200 \
     --selection-pattern "-" \
     --output table
-
+if [[ $exit_status -eq 0 ]]; then
+    echo -e "$INFO Successfully created API integration response."
+else
+    echo -e "$ERROR Cannot create API integration response. Exiting."
+    exit 1
+fi
 
 # Adding permission for internal calls
 echo -e "$INFO API add-permission for internal calls." 
@@ -118,7 +151,12 @@ aws lambda add-permission \
     --principal apigateway.amazonaws.com \
     --statement-id api-lambda-permission-1 \
     --action lambda:InvokeFunction 
-
+if [[ $exit_status -eq 0 ]]; then
+    echo -e "$INFO Successfully added permission for internal calls."
+else
+    echo -e "$ERROR Cannot add permission for internal calls. Exiting."
+    exit 1
+fi
 
 # Adding permission for external calls
 echo -e "$INFO API add-permission for external calls." 
@@ -128,6 +166,14 @@ aws lambda add-permission \
     --principal apigateway.amazonaws.com \
     --statement-id api-lambda-permission-2 \
     --action lambda:InvokeFunction 
+
+if [[ $exit_status -eq 0 ]]; then
+    echo -e "$INFO Successfully added permission for external calls."
+else
+    echo -e "$ERROR Cannot add permission for external calls. Exiting."
+    exit 1
+fi
+
 
 echo -e "$INFO Finished creating $(FC $API_HTTP_METHOD) under" \
     "$(FC $API_RESOURCE_NAME) resource" 
